cee74afcb5d3dea6f66b118a806ecc3868102dfa
somaSeb <133811635+somaSeb@users.noreply.github.com>
2024-01-12 14:07:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import Form from "./components/Form/Form";
 import React from "react";
+import { uid } from "uid";
+import { useState } from "react";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(newActivity) {
+    const newActivityWithId = { ...newActivity, id: uid() };
+    setActivities((previousActivities) => [
+      ...previousActivities,
+      newActivityWithId,
+    ]);
+  }
+
+  // setActivitiesetEntries([{ id: uid(), date, ...newActkvk}, ...entries]);
+
   return (
     <>
       <h1>Weather App</h1>
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </>
   );
 }
